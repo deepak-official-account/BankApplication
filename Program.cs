c7f59eb7f5022d65645ae363c10e44187ac73ec1
thinksysuser <thinksysuser@CPU-0146>
2025-02-21 14:21:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Principal;
+
+namespace BankApplication
+{
+    internal class Program
+    {
+//        1. Deposit Money 
+//2. Withdraw Money 
+//3. Check Balance
+//4. Account Details 
+//5. Exit
+        private int choice;
+        private string name;
+        private string accountNumber;
+        private double depositAmount;
+        void GetUserInformation()
+        {
+
+            Console.WriteLine("Welcome to Simple Bank");
+            Console.WriteLine("Please Enter your Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please Enter your Account Number");
+            accountNumber = Console.ReadLine();
+            Console.WriteLine("Please Enter Initial Deposit Amount");
+            depositAmount = Convert.ToDouble(Console.ReadLine());
+
+
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+
+            } while ();
+        }
+    }
+}
